9455edca47a8793d4d995c5e4e0d926fa343e9e3
Hồng Nguyên <95576426+hongnguyen2011@users.noreply.github.com>
2024-06-10 22:01:51 +0700

    add generate JWT token

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -5,7 +5,10 @@ using BookStore_API.Utility;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Net;
 
 namespace RedMango_API.Controllers
@@ -46,12 +49,29 @@ namespace RedMango_API.Controllers
                 return BadRequest(_response);
             }
 
-  
+            // generate JWT token 
+            var roles = await _userManager.GetRolesAsync(userFromDb);
+            JwtSecurityTokenHandler tokenHandler = new();
+            byte[] key = Encoding.ASCII.GetBytes(secretKey);
 
+            SecurityTokenDescriptor tokenDescriptor = new()
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim("fullName", userFromDb.Name),
+                    new Claim("id", userFromDb.Id.ToString()),
+                    new Claim(ClaimTypes.Email, userFromDb.UserName.ToString()),
+                    new Claim(ClaimTypes.Role, roles.FirstOrDefault()),
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
             LoginResponseDTO loginResponse = new()
             {
                 Email = userFromDb.Email,
-                Token = "token"
+                Token = tokenHandler.WriteToken(token)
             };
 
             if (loginResponse.Email == null || string.IsNullOrEmpty(loginResponse.Token))
