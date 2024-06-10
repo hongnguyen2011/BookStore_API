1a82593e3bf4edde72e8c384eb084658534a2114
Hồng Nguyên <95576426+hongnguyen2011@users.noreply.github.com>
2024-06-10 22:06:28 +0700

    add authentiation and authorization endpoints

---
+++ Controllers/AuthTestController.cs
@@ -0,0 +1,27 @@
+﻿using BookStore_API.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore_API.Controllers
+{
+        [Route("api/AuthTest")]
+        [ApiController]
+        public class AuthTestController : ControllerBase
+        {
+            [HttpGet]
+            [Authorize]
+            public async Task<ActionResult<string>> GetSomething()
+            {
+                return "You are authenticated";
+            }
+
+            [HttpGet("{id:int}")]
+            [Authorize(Roles = SD.Role_Admin)]
+            public async Task<ActionResult<string>> GetSomething(int someIntValue)
+            {
+                //authorization -> Authentication + Some access/roles
+                return "You are Authorized with Role of Admin";
+            }
+        }
+    
+}
